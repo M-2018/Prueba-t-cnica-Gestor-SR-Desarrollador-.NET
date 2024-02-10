9013310bdcfbeb9b055f7a2ed200f4a9c91dd0f3
Araneus <kbasara69@gmail.com>
2024-02-10 13:39:10 -0500

    Ejercicios terminados

---
+++ Funcionalidad_1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public interface IMediaAgent
+{
+    double GetMedia(List<double> data);
+}
+
+public class AritmeticaAgent : IMediaAgent
+{
+    public double GetMedia(List<double> data)
+    {
+        double media = data.Sum() / data.Count;
+        Console.WriteLine($"Media Aritmética: {media}");
+        return media;
+    }
+}
+
+public class HarmonicaAgent : IMediaAgent
+{
+    public double GetMedia(List<double> data)
+    {
+        double media = 0;
+        foreach (var number in data)
+        {
+            media += 1 / number;
+        }
+        media = data.Count / media;
+        Console.WriteLine($"Media Harmonica: {media}");
+        return media;
+    }
+}
+
+public class MedianaAgent : IMediaAgent
+{
+    public double GetMedia(List<double> data)
+    {
+        data.Sort();
+        int n = data.Count;
+
+        double mediana;
+        if (n % 2 == 0)
+        {
+            mediana = (data[n / 2 - 1] + data[n / 2]) / 2.0;
+        }
+        else
+        {
+            mediana = data[n / 2];
+        }
+
+        Console.WriteLine($"Mediana: {mediana}");
+        return mediana;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<double> data = new List<double> { 4, 2, 8, 6, 7, 3, 1, 5 };
+
+        AritmeticaAgent aritmeticaAgent = new AritmeticaAgent();
+        HarmonicaAgent harmonicaAgent = new HarmonicaAgent();
+        MedianaAgent medianaAgent = new MedianaAgent();
+
+        double aritmeticmedia = aritmeticaAgent.GetMedia(data);
+        double harmonicmedia = harmonicaAgent.GetMedia(data);
+        double median = medianaAgent.GetMedia(data);
+
+        Console.ReadLine();
+    }
+}
---
+++ Funcionalidad_2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+public class Escalera
+{
+    public static string GetStaircaseA(int n)
+    {
+        if (n <= 0 || n >= 100)
+        {
+            throw new ArgumentException("El tamaño de la escalera debe estar en el rango 0 < n < 100.");
+        }
+
+        string staircase = "";
+        for (int i = 1; i <= n; i++)
+        {
+            string spaces = new string(' ', n - i);
+            string symbols = new string('#', i);
+            staircase += spaces + symbols + "\n";
+        }
+
+        return staircase;
+    }
+
+    public static string GetStaircaseB(int n)
+    {
+        if (n <= 0 || n >= 100)
+        {
+            throw new ArgumentException("El tamaño de la escalera debe estar en el rango 0 < n < 100.");
+        }
+
+        string staircase = "";
+        for (int i = n; i >= 1; i--)
+        {
+            string spaces = new string(' ', n - i);
+            string symbols = new string('#', i);
+            staircase += spaces + symbols + "\n";
+        }
+
+        return staircase;
+    }
+
+    public static string GetStaircaseC(int n)
+    {
+        if (n <= 0 || n >= 100)
+        {
+            throw new ArgumentException("El tamaño de la escalera debe estar en el rango 0 < n < 100.");
+        }
+
+        string staircase = "";
+        for (int i = 1; i <= n; i++)
+        {
+            string spaces = new string(' ', n - i);
+            string symbols = new string('#', 2 * i - 1);
+            staircase += spaces + symbols + "\n";
+        }
+
+        for (int i = n - 1; i >= 1; i--)
+        {
+            string spaces = new string(' ', n - i);
+            string symbols = new string('#', 2 * i - 1);
+            staircase += spaces + symbols + "\n";
+        }
+
+        return staircase;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Escalera A:\n" + GetStaircaseA(4));
+        Console.WriteLine("Escalera B:\n" + GetStaircaseB(4));
+        Console.WriteLine("Escalera C:\n" + GetStaircaseC(4));
+    }
+}
